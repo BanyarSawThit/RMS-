80a4f1f5bfa0f6c586dd38b51e146ded1b31eada
BanyarSawThit <banyarsawthit1922017@gmail.com>
2025-08-10 22:13:44 +0800

    update settings.py

--- RMS/settings.py
+++ RMS/settings.py
@@ -27,7 +27,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'rms-ktdo.onrender.com']
 
 
 # Application definition
